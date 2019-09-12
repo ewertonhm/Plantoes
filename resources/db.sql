b42a92f0036656574c64c5ed02d5210717d5e033
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-09-11 22:15:40 -0300

    db scheme

--- resources/db.sql
+++ resources/db.sql
@@ -1,1 +1,43 @@
 /*create database plantoes; */
+
+CREATE TABLE cidade(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255)
+);
+
+CREATE TABLE feriado(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    data DATE,
+    cidade_id INTEGER REFERENCES cidade(id)
+);
+
+CREATE TABLE juiz(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    cidade_id INTEGER REFERENCES cidade(id)
+);
+
+CREATE TABLE usuario(
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(32),
+    password VARCHAR(32)
+);
+
+CREATE TABLE diaindisponivel(
+    id SERIAL PRIMARY KEY,
+    data DATE,
+    juiz_id INTEGER REFERENCES juiz(id)
+);
+
+CREATE TABLE diaprefererivel(
+    id SERIAL PRIMARY KEY,
+    data DATE,
+    juiz_id INTEGER REFERENCES juiz(id)
+);
+
+CREATE TABLE plantao(
+    id SERIAL PRIMARY KEY,
+    data DATE,
+    juiz_id INTEGER REFERENCES juiz(id)
+);
