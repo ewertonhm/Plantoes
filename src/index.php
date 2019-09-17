ae7fe41c14213f2a9040c23335736a53c0a2e8dd
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-09-17 11:45:33 -0300

    fonts local hosted

--- src/index.php
+++ src/index.php
@@ -54,7 +54,7 @@ if(!$login->isLogged()){
     <link rel="canonical" href="http://www.example.com/">
     -->
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=pt-br">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-light_blue.min.css">
     <link rel="stylesheet" href="assets/styles.css">
