af6d1d5fa00f86d9706d2aef79390db8206e9edf
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-09-18 00:49:52 -0300

    login em MDL

--- src/login.php
+++ src/login.php
@@ -2,24 +2,31 @@
 
 require_once 'vendor/autoload.php';
 
+use App\View\LayoutPadrao;
 use App\View\Login;
 
 $login = new \App\Controller\Login();
 if($login->isLogged()){
     header('location: index.php');
 } else {
+    $layout = new LayoutPadrao();
+    $layout->inicio('Login');
     $body = new Login();
+    $layout->fim();
     if(isset($_POST['btn-login'])){
         $usuario = $login->login($_POST['login'],$_POST['password']);
         if(isset($usuario) AND $usuario != false){
             $id = $usuario->getId();
         }
         if(isset($id) AND $id != NULL){
-            header('location: index.php');
+            echo "<script>";
+            echo "location.reload();";
+            echo "</script>";
+        } else {
+            echo "<script>";
+            echo "alert('Login ou Senha incorreto!');";
+            echo "</script>";
         }
-        echo "<script>";
-        echo "alert('Login ou Senha incorreto!');";
-        echo "</script>";
     }
 }
 ?>
